9f34af2a953853e7b3b8dbe856c6aa71de03dc86
Ephraim <ephraim@modevx.com>
2025-03-26 08:25:56 -0400

    create D3 county map

--- src/components/D3Maps.js
+++ src/components/D3Maps.js
@@ -5,7 +5,21 @@ import albersTopoJson from "@/json/topojson-albers-map.json";
 const albersProjection = geoAlbers();
 const albersGeoPath = geoPath(albersProjection);
 
-export const D3UsMap = ({ children }) => {
+export const AlbersCountyMap = ({ children }) => {
+  const counties = topojsonClient.feature(albersTopoJson, "counties");
+
+  return (
+    <div className="w-full h-full">
+      <svg viewBox="0 -60 975 610" xmlns="http://www.w3.org/2000/svg">
+        <path d={albersGeoPath(counties)} stroke="white" fill="grey" />
+
+        {children}
+      </svg>
+    </div>
+  );
+};
+
+export const AlbersStateMap = ({ children }) => {
   const states = topojsonClient.feature(albersTopoJson, "states");
 
   return (
